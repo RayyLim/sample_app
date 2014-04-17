fced4b7ff1450ea3ea9c6652e521eb5f5ce43584
rlim <rayylim@gmail.com>
2014-04-17 00:20:26 -0700

    update .gitignore, and secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,16 @@
-# Be sure to restart your server when you modify this file.
+require 'securerandom'
 
-# Your secret key is used for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
 
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-SampleApp::Application.config.secret_key_base = 'facef45f28b70a7d141e94d88f12633688f6260af11b175ce92c62c05cd086711c18c0e0b9bcbadab3b75ff24a8b19ab0d2a7e4decda1c0037a54bbd981aee2a'
+SampleApp::Application.config.secret_key_base = secure_token
